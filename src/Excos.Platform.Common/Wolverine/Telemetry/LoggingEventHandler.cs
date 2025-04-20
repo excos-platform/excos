67066b2c8841ed72159d6a7dd5ed243eaa3b2489
Marian Dziubiak <marian.dziubiak@gmail.com>
2025-04-20 13:47:22 +0100

    Cleanup

--- src/Excos.Platform.Common/Wolverine/Telemetry/LoggingEventHandler.cs
+++ src/Excos.Platform.Common/Wolverine/Telemetry/LoggingEventHandler.cs
@@ -9,59 +9,58 @@ using Microsoft.Extensions.Logging;
 using Wolverine;
 using Wolverine.Configuration;
 
-namespace Excos.Platform.Common.Wolverine.Telemetry
-{
-	public static class LoggingEventHandlerExtensions
-	{
-		/// <summary>
-		/// Add an event handler which will log event details.
-		/// </summary>
-		/// <typeparam name="T">Event type</typeparam>
-		public static HandlerDiscovery AddLogger<T>(this HandlerDiscovery discovery)
-		{
-			return discovery.IncludeType(typeof(LoggingEventHandler<T>));
-		}
-	}
+namespace Excos.Platform.Common.Wolverine.Telemetry;
 
+public static class LoggingEventHandlerExtensions
+{
 	/// <summary>
-	/// An event hadnler which logs event details.
+	/// Add an event handler which will log event details.
 	/// </summary>
 	/// <typeparam name="T">Event type</typeparam>
-	public static class LoggingEventHandler<T>
+	public static HandlerDiscovery AddLogger<T>(this HandlerDiscovery discovery)
 	{
-		public static void Handle(T message, Envelope envelope, ILogger<T> logger, PrivacyValueRedactor redactor)
+		return discovery.IncludeType(typeof(LoggingEventHandler<T>));
+	}
+}
+
+/// <summary>
+/// An event hadnler which logs event details.
+/// </summary>
+/// <typeparam name="T">Event type</typeparam>
+public static class LoggingEventHandler<T>
+{
+	public static void Handle(T message, Envelope envelope, ILogger<T> logger, PrivacyValueRedactor redactor)
+	{
+		if (!logger.IsEnabled(LogLevel.Information) || message is null)
 		{
-			if (!logger.IsEnabled(LogLevel.Information) || message is null)
-			{
-				return;
-			}
-
-			var attributes = new ActivityTagsCollection()
-			{
-				{ "EnvelopeId", envelope.Id },
-				{ "ConversationId", envelope.ConversationId == Guid.Empty ? null : envelope.ConversationId },
-				{ "TenantId", envelope.TenantId },
-				{ "Source", envelope.Source },
-				{ "SentAt", envelope.SentAt },
-				{ "SagaId", envelope.SagaId },
-			};
-
-			List<PrivacyValueDescriptor> privacyDescriptors = PrivacyValueDescriptor.GetDescriptors(typeof(T));
-			var dataAttributes = new Dictionary<string, object?>(privacyDescriptors.Count);
-
-			foreach (PrivacyValueDescriptor descriptor in privacyDescriptors)
-			{
-				dataAttributes.Add(descriptor.OpenTelemetryName, redactor.Redact(descriptor.GetValue(message), descriptor.Redaction));
-			}
-
-			attributes.Add("Data", JsonSerializer.Serialize(dataAttributes));
-
-			logger.Log(
-				LogLevel.Information,
-				new EventId(0, typeof(T).FullNameInCode()),
-				attributes,
-				exception: null,
-				static (attr, _) => $"{typeof(T).FullNameInCode()}: {attr["Data"]}");
+			return;
 		}
+
+		var attributes = new ActivityTagsCollection()
+		{
+			{ "EnvelopeId", envelope.Id },
+			{ "ConversationId", envelope.ConversationId == Guid.Empty ? null : envelope.ConversationId },
+			{ "TenantId", envelope.TenantId },
+			{ "Source", envelope.Source },
+			{ "SentAt", envelope.SentAt },
+			{ "SagaId", envelope.SagaId },
+		};
+
+		List<PrivacyValueDescriptor> privacyDescriptors = PrivacyValueDescriptor.GetDescriptors(typeof(T));
+		var dataAttributes = new Dictionary<string, object?>(privacyDescriptors.Count);
+
+		foreach (PrivacyValueDescriptor descriptor in privacyDescriptors)
+		{
+			dataAttributes.Add(descriptor.OpenTelemetryName, redactor.Redact(descriptor.GetValue(message), descriptor.Redaction));
+		}
+
+		attributes.Add("Data", JsonSerializer.Serialize(dataAttributes));
+
+		logger.Log(
+			LogLevel.Information,
+			new EventId(0, typeof(T).FullNameInCode()),
+			attributes,
+			exception: null,
+			static (attr, _) => $"{typeof(T).FullNameInCode()}: {attr["Data"]}");
 	}
 }
