6caf6bb6f0f9dfc51cb627e308e6e783ba776e52
Marian Dziubiak <marian.dziubiak@gmail.com>
2025-05-12 08:30:39 +0100

    Bump WaitForEvents time

--- test/Excos.Testing.OpenTelemetry/TestOtlpServer.cs
+++ test/Excos.Testing.OpenTelemetry/TestOtlpServer.cs
@@ -68,7 +68,7 @@ public class TestOtlpServer
 	{
 		// We will wait for up to 600ms for any trace or logs to come in
 		long currentCounterValue = Interlocked.Read(ref this.updateCounter);
-		int tries = 3;
+		int tries = 5;
 		do
 		{
 			await Task.Delay(200); 
