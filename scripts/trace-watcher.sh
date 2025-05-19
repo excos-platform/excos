5e63239ac08d995f6306910fb7c89eaa8ebddb8f
Marian Dziubiak <marian.dziubiak@gmail.com>
2025-05-19 07:51:29 +0100

    Fix awk

--- scripts/trace-watcher.sh
+++ scripts/trace-watcher.sh
@@ -7,7 +7,8 @@ PROCESS_NAME="$1"
 dotnet tool install --global dotnet-trace
 
 while true; do
-    PID=$(dotnet trace ps | grep -E "$PROCESS_NAME(\s|$|\.)" | awk '{ print $1 }')
+	# grep output for all matches and then take first pid
+    PID=$(dotnet trace ps | grep -E "$PROCESS_NAME(\s|$|\.)" | awk '{ print $1; exit  }')
     if [[ -n "$PID" ]]; then
         echo "Found PID: $PID"
         dotnet trace collect --process-id "$PID" --output "${PROCESS_NAME}_trace.nettrace" --providers "OpenTelemetry-Exporter-OpenTelemetryProtocol"
