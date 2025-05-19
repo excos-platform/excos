de4ab8c0f37b2731206b5b9f30d0ea02423963f7
Marian Dziubiak <marian.dziubiak@gmail.com>
2025-05-19 07:00:04 +0100

    Try capture trace during test to see why it doesn't work

---
+++ scripts/trace-watcher.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Usage: ./trace-watcher.sh <process_name>
+
+PROCESS_NAME="$1"
+
+while true; do
+    PID=$(dotnet-trace ps | awk -v name="$PROCESS_NAME" '$0 ~ name { print $1; exit }')
+    if [[ -n "$PID" ]]; then
+        echo "Found PID: $PID"
+		# View with chrome://tracing
+        dotnet-trace collect --process-id "$PID" --format Chromium --output "${PROCESS_NAME}_trace.json"
+        break
+    fi
+    sleep 1
+done
