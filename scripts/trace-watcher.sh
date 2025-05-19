33ca55a09e59b3e5bb53986f0ae92da2a2634238
Marian Dziubiak <marian.dziubiak@gmail.com>
2025-05-19 07:23:06 +0100

    Use nettrace and specify OTEL provider we want to listen to

--- scripts/trace-watcher.sh
+++ scripts/trace-watcher.sh
@@ -10,8 +10,7 @@ while true; do
     PID=$(dotnet trace ps | awk -v name="$PROCESS_NAME" '$0 ~ name { print $1; exit }')
     if [[ -n "$PID" ]]; then
         echo "Found PID: $PID"
-		# View with chrome://tracing
-        dotnet trace collect --process-id "$PID" --format Chromium --output "${PROCESS_NAME}_trace.json"
+        dotnet trace collect --process-id "$PID" --output "${PROCESS_NAME}_trace.nettrace" --providers "OpenTelemetry-Exporter-OpenTelemetryProtocol"
         break
     fi
     sleep 1
