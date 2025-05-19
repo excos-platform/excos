5afa2d2897ed93b461531e7f12fa414734ecb21e
Marian Dziubiak <marian.dziubiak@gmail.com>
2025-05-19 07:45:14 +0100

    Rework the trace finding pid such that Tests project is not used

--- scripts/trace-watcher.sh
+++ scripts/trace-watcher.sh
@@ -7,7 +7,7 @@ PROCESS_NAME="$1"
 dotnet tool install --global dotnet-trace
 
 while true; do
-    PID=$(dotnet trace ps | awk -v name="$PROCESS_NAME" '$0 ~ name { print $1; exit }')
+    PID=$(dotnet trace ps | grep -E "$PROCESS_NAME(\s|$|\.)" | awk '{ print $1 }')
     if [[ -n "$PID" ]]; then
         echo "Found PID: $PID"
         dotnet trace collect --process-id "$PID" --output "${PROCESS_NAME}_trace.nettrace" --providers "OpenTelemetry-Exporter-OpenTelemetryProtocol"
